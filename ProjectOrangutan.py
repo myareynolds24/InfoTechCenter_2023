8018f94f167cb068273fee160b2b292ce1896bb9
Mya <02mya.reynolds@gulllakecs.org>
2023-10-17 08:31:08 -0400

    Add Description and Welcome Print Lines
    Wow...

---
+++ ProjectOrangutan.py
@@ -0,0 +1,6 @@
+'''
+Our Welcome Screen will start our program letting drivers know that the InfoTech Center 2023 is loading
+'''
+
+print("\n\nWelcome - InfoTech Center 2023")
+print("\nInfoTech Center 2023 is loading...")
