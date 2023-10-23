43a4805027e9ec821eda278c5a6fddbe5995eac4
Mya <02mya.reynolds@gulllakecs.org>
2023-10-23 08:23:17 -0400

    Add Time Library/Variable Thing for Time To Sleep
    "The Kraken of Gits"

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -2,5 +2,11 @@
 Our Welcome Screen will start our program letting drivers know that the InfoTech Center 2023 is loading
 '''
 
+#Import Libraries Here
+import time
+
+timetosleep = 2
+
 print("\n\nWelcome - InfoTech Center 2023")
+time.sleep(timetosleep)
 print("\nInfoTech Center 2023 is loading...")
